809794c975948d6185a8bcfc809e5b5da887f9c1
Ashwin <ashwinathappank@gmail.com>
2024-04-24 10:25:26 +0530

    01-js-hard-calculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,111 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+    result = 0;
+
+    add(x) {
+        this.result += x;
+    }
+
+    subtract(x) {
+        this.result -= x;
+    }
+
+    multiply(x) {
+        this.result *= x;
+    }
+
+    divide(x) {
+        if (x === 0) {
+            throw new Error("Cannot divide by 0");
+        }
+        this.result /= x;
+    }
+
+    clear() {
+        this.result = 0;
+    }
+
+    getResult() {
+        return this.result;
+    }
+
+    calculate(expression) {
+        if (expression === '') {
+            this.result = 0;
+            return;
+        }
+
+        let operands = [];
+        let operators = [];
+
+        let tokens = expression.trim().split("");
+        tokens = tokens.filter((x) => x !== ' ');
+
+        for (let i = 0; i < tokens.length; i++) {
+            if (Number.isInteger(parseInt(tokens[i]))) {
+                let num = '';
+                while (i < tokens.length && (Number.isInteger(parseInt(tokens[i])) || tokens[i] === '.')) {
+                    num += tokens[i++];
+                }
+                operands.push(Number.parseFloat(num));
+                i --;
+            } else if (tokens[i] === '(') {
+                operators.push(tokens[i]);
+            } else if (tokens[i] === ')') {
+                while (operators[operators.length - 1] !== '(') {
+                    let ans = this.calc(operators, operands);
+                    operands.push(ans);
+                }
+                operators.pop();
+            } else if (tokens[i] === '+' || tokens[i] === '-' || tokens[i] === '*' || tokens[i] === '/') {
+                if (operators.length !== 0 && this.precedence(tokens[i]) <= this.precedence(operators[operators.length - 1])) {
+                    let ans = this.calc(operators, operands);
+                    operands.push(ans);
+                }
+                operators.push(tokens[i]);
+            }
+        }
+
+        while (operators.length !== 0) {
+            let ans = this.calc(operators, operands)
+            operands.push(ans);
+        }
+
+        this.result = operands.pop();
+    }
+
+    precedence(op) {
+        switch (op) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+
+    calc(operators, operands) {
+        let a = operands.pop();
+        let b = operands.pop();
+        let o = operators.pop();
+
+        switch (o) {
+            case '+': return b + a;
+            case '-': return b - a;
+            case '*': return b * a;
+            case '/':
+                if (a === 0) {
+                    throw new Error('Cannot Divide by 0');
+                }
+                return b / a;
+            default: throw new Error("Invalid operator");
+        }
+    }
+}
 
 module.exports = Calculator;
